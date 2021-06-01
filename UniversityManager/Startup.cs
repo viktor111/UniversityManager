a0bd1abbe866fe6c0bb8e9825e2744999d377fdd
viktor111 <Cimfiw-zexboc-jocqe1>
2021-06-01 16:17:59 +0300

    TEST

--- UniversityManager/Startup.cs
+++ UniversityManager/Startup.cs
@@ -25,7 +25,6 @@ namespace UniversityManager
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -44,7 +43,6 @@ namespace UniversityManager
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -55,7 +53,6 @@ namespace UniversityManager
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
