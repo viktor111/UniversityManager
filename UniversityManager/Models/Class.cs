b354769c55fcc6c0f6fc465bd1c8a7105a82331c
george <alienguymc@gmail.com>
2021-06-01 17:31:16 +0300

    create class and semester models

---
+++ UniversityManager/Models/Class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniversityManager.Models
+{
+    public class Class
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime BeginDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string TeacherName { get; set; }
+
+        [Required]
+        [MaxLength(6)]
+        public string Room { get; set; }
+    }
+}
